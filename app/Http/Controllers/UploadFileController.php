346589ed0244e7a57a4c8581019088cd842d1cdc
崎 <slight@kilingzhang.com>
2018-06-18 01:38:20 +0800

    upload
    upload

--- app/Http/Controllers/UploadFileController.php
+++ app/Http/Controllers/UploadFileController.php
@@ -16,53 +16,33 @@ class UploadFileController extends Controller
     public function store(Request $request)
     {
 
-        $config = [
-            'region' => env('COS_REGION'),
-            'credentials' => [
-                'appId' => env('COS_APPID'),
-                'secretId' => env('COS_KEY'),
-                'secretKey' => env('COS_SECRET')
-            ]
-        ];
-        $cosClient = new Client($config);
-        //上传文件
+        if (!$request->hasFile('file')) {
+            return response()->json([], 500, '无法获取上传文件');
+        }
+        $file = $request->file('file');
 
+        if ($file->isValid()) {
+            // 获取文件相关信息
+            $originalName = $file->getClientOriginalName(); // 文件原名
+            $ext = $file->getClientOriginalExtension();     // 扩展名
+            $realPath = $file->getRealPath();   //临时文件的绝对路径
+            $type = $file->getClientMimeType();     // image/jpeg
 
-        //
-        $img = $request->input(['uploaderFiles'], '');
+            // 上传文件
+            $filename = date('Ymd/His');
+            // 使用我们新建的uploads本地存储空间（目录）
+            $path = $file->store($filename, 'uploads');
+            return response()->json([
+                'status_code' => 200,
+                'message' => 'success',
+                'photo' => $path,
+                'name' => $originalName,
+            ]);
 
-        // 获取图片
-        list($type, $data) = explode(',', $img);
-
-        // 判断类型
-        if (strstr($type, 'image/jpeg') != '') {
-            $ext = '.jpg';
-        } elseif (strstr($type, 'image/gif') != '') {
-            $ext = '.gif';
-        } elseif (strstr($type, 'image/png') != '') {
-            $ext = '.png';
+        } else {
+            return response()->json([], 500, '文件未通过验证');
         }
 
-        // 生成的文件名
-        $name = time() . $ext;
-
-        try {
-            $result = $cosClient->upload(
-            //bucket的命名规则为{name}-{appid} ，此处填写的存储桶名称必须为此格式
-                $bucket = 'outsourcing-1256239514',
-//                $bucket = 'outsourcing-wx-jingduo-1251794938',
-                $key = $name,
-                $body = base64_decode($data)
-            );
-
-            $img = $result->get("Location");
-        } catch (\Exception $e) {
-            return $e;
-        }
-
-
-        $res = array('img' => $img);
-        return $res;
     }
 
 }
